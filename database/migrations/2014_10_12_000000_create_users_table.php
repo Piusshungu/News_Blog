a2a00a2616243b3a4ad65759a679ff5b2cf823ce
Piusshungu <shungupius@gmail.com>
2021-11-15 22:53:11 +0300

    updated the package for the user IDs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Emadadly\LaravelUuid\Uuids;
 
 class CreateUsersTable extends Migration
 {
@@ -14,7 +15,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->primary('uuid');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
