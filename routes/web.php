467d0a562de9dc5ff843a2da834d753b3bb2d762
Piusshungu <shungupius@gmail.com>
2021-11-18 22:09:47 +0300

    Added the relation between the Eloquent Model Post and corresponding category

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,7 @@ Route::get('/', function () {
 });
 
 
-Route::get('posts/{post}', function($id) {
-
-    $post = Post::findOrFail($id);
+Route::get('posts/{post:slug}', function(Post $post) {
     
     return view('post', [
         'post' => $post
