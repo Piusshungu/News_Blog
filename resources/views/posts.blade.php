87ddeca5b71ebacf91ce683fe33038ef614c4208
Piusshungu <shungupius@gmail.com>
2021-12-04 11:06:58 +0300

    Added a route to get all the posts written by specific user

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
 <h1><a href="/posts/{{ $post->slug }}">{{$post->title}}</a></h1>
 
 <p>
-By <a href="#">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a>
+By <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a>
 </p>
 
 <div>
