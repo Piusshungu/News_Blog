6bf6e8129d682002bc8cb11b5be779be1712f2e2
Piusshungu <shungupius@gmail.com>
2021-12-09 14:25:19 +0300

    Looping the post cards to accommodate new posts and old ones

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,47 +4,33 @@
 
  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
+ @if($posts->count())
+
 <x-post-featured-card :post="$posts[0]" />
+
+@if ($posts->count() > 1)
           
 
 <div class="lg:grid lg:grid-cols-2">
-                
- <x-post-card/>
 
-<x-post-card/>
+@foreach ($posts->skip(1) as $post)
+    
+                
+ <x-post-card :post="$post" class="bg-red-500"/>
+
+@endforeach
 
 </div>
 
-<div class="lg:grid lg:grid-cols-3">
+@endif
 
-<x-post-card/>
+@else
 
-<x-post-card/>
+<p class="text-center">No Post yet. Please check again later</p>
 
- <x-post-card/>
-                
- </div>
+@endif
+
 
 </main>
-
-<!-- @foreach ($posts as $post)
-
-<article>
-
-<h1><a href="/posts/{{ $post->slug }}">{{$post->title}}</a></h1>
-
-<p>
-By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name }}</a>
-</p>
-
-<div>
-
- {{$post->excerpt}} 
-   
-</div>
-
-</article>
-
-@endforeach -->
 
 </x-layout>
