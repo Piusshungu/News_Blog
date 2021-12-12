bc39c5b45ee9bdc30d3f771a0bdcc3c979469ce7
Piusshungu <shungupius@gmail.com>
2021-12-12 11:28:57 +0300

    Changed the category button to a component to avoid duplicates codes

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -13,9 +13,7 @@
                                     <a href="/categories/{{ $post->category->slug }}"
                                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                                        style="font-size: 10px">Techniques</a>
-                                    <a href="#"
-                                       class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">{{ $post->category->name }}</a>
+                                    <x-category-button :category:$post="category"/>
                                 </div>
 
                                 <div class="mt-4">
